9d5c82f40e53c773d939a1295153c397fb8eef77
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-11-23 16:31:21 +0700

    update

--- week2/13/go.2.13.go
+++ week2/13/go.2.13.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func ()  {
-	fmt.Println(strings.Split("a,b,c,d,e",","))
-	fmt.Println(strings.Split("hello-i-am-goku","o"))
+func main() {
+	fmt.Println(strings.Split("a,b,c,d,e", ","))
+	fmt.Println(strings.Split("hello-i-am-goku", "o"))
 }
