aee07c795b7872e82d23b8ea50afdf06b36d986a
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-11-23 15:53:30 +0700

    update

--- week2/11/go.2.11.go
+++ week2/11/go.2.11.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 )
 
-func main()  {
-	fmt.Println(strings.Index("Hello Wollo","o"))
-	fmt.Println(strings.LastIndex("Hello world","O"))
+func main() {
+	fmt.Println(strings.Index("Hello Wollo", "o"))
+	fmt.Println(strings.LastIndex("Hello world", "O"))
+}
