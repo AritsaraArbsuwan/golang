144d4f677726ff13d7f8c1b8c3e739e3d4e1deeb
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-11-23 17:17:41 +0700

    update

--- week2/16/go2.16.go
+++ week2/16/go2.16.go
@@ -2,9 +2,10 @@ package main
 
 import "fmt"
 
-func  main()  {
-	fmt.Println(3+2)
-	fmt.Println(3-2.199)
-	fmt.Println(3.0*2.0)
-	fmt.Println(3.0/2.0)
+func main() {
+	fmt.Println(3 + 2)
+	fmt.Println(3 - 2.199)
+	fmt.Println(3.0 * 2.0)
+	fmt.Println(3.0 / 2.0)
 	fmt.Println(3 % 2)
+}
