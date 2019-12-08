3bd3846dd9f97684a799d4078418e0ea6905cb18
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-08 18:37:42 +0700

    Update go5.6.go

--- week5/go5.6.go
+++ week5/go5.6.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	alphabets := [2][3]string{{"a", "b", "c"}}
+	alphabets := [2][3]string{{"a", "b", "c"},{"A","B","c"}}
 	fmt.Println(alphabets)
 	fmt.Println(alphabets[0][1])
 	number := [2][3][2]int{
