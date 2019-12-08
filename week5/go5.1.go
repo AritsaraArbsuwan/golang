1c9c7c4105dd537c232bbac512008d19e789620e
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-08 17:35:57 +0700

    Update go5.1.go

--- week5/go5.1.go
+++ week5/go5.1.go
@@ -1,1 +1,19 @@
 package main
+
+import "fmt"
+
+func main() {
+	student_1 := "Boat"
+	student_2 := "Arit"
+	student_3 := "Nan"
+	student_4 := "Khet"
+	student_5 := "Tan"
+	student_6 := "Aum"
+	student_7 := "Meena"
+	student_8 := "Ter"
+	student_9 := "kao"
+	student_10 := "Bam"
+
+	fmt.Println(student_1, student_2, student_3, student_4, student_5, student_6, student_7, student_8, student_9, student_10)
+
+}
