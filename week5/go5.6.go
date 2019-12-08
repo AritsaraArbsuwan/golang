d81e03acd745fb6ea8897cc5748867a6a9c58d88
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-08 18:39:55 +0700

    Update go5.6.go

--- week5/go5.6.go
+++ week5/go5.6.go
@@ -6,7 +6,7 @@ func main() {
 	alphabets := [2][3]string{{"a", "b", "c"}, {"A", "B", "c"}}
 	fmt.Println(alphabets)
 	fmt.Println(alphabets[0][1])
-	number := [2][3][2]int{
+	numbers := [2][3][2]int{
 		{
 			{1, 2},
 			{10, 20},
