75f5e64c8a2a74aa21d59d2384176bc144c2a9a7
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-08 23:44:37 +0700

    Update go5.14.go

--- week5/go5.14.go
+++ week5/go5.14.go
@@ -1,6 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	humans := []string{"Arit", "Boat", "Doremon"}
+	names := []string{"Doreme", "Dum"}
+	names = append(names, humans...)
+	fmt.Println(names)
 
 }
