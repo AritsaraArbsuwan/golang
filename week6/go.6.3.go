21e80bc6de1360cb1cdc1f1ad7110d1f0b589842
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-15 19:03:20 +0700

    Update go.6.3.go

--- week6/go.6.3.go
+++ week6/go.6.3.go
@@ -1,9 +1,19 @@
 package main
 
+import "fmt"
+
 func sum(numbres ...int) int {
 	total := 0
 	for _, n := range numbres {
 		total = total + n
 	}
-	return total
+	return total	
+}
+func main()  {
+	x := sun(1, 3, ,5 ,7 ,9)
+	fmt.Println(x)
+
+	y:= sun()
+	fmt.Println(y)
+	
 }
