3f47b60beefb6362eecd88913a6b688adbbf9573
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-15 19:51:06 +0700

    Create go7.6.go

---
+++ week6/go7.6.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func  say(greet string) func (string)string {
+	return func (name string)string {
+         return greet + name		
+	}
+}
+		
+func  main()  {
+	x := say("Hello")
+	
+}	}
+	
+}
+
