146a1e8d04e4ee5c94f2716b1c032865fd428aec
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-15 20:04:42 +0700

    Update go7.7.go

--- week6/go7.7.go
+++ week6/go7.7.go
@@ -2,13 +2,17 @@ package main
 
 import "fmt"
 
-func  makeEvan() func () int {
+func makeEvan() func() int {
 	even := 0
-	return func () int {
+	return func() int {
 		even = even + 2
 		return even
 	}
-	
-}  {
-	
+}
+
+func main() {
+	nextEven := makeEvan()
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
+	fmt.Println(nextEven())
 }
