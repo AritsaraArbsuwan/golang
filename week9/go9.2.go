07dc1d2440200c5df157b9aea8e2b31833d66752
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-29 19:11:25 +0700

    Update go9.2.go

--- week9/go9.2.go
+++ week9/go9.2.go
@@ -2,23 +2,23 @@ package main
 
 import "fmt"
 
-type student student {
-	name string
-	age int
+type student struct {
+	name  string
+	age   int
 	email string
 }
 
-func main(){
+func main() {
 	var a student
 	a.name = "Goku"
-	a.age  = 18
+	a.age = 18
 	a.email = "Goku@super.saiya"
 
-	b :=student{"Gohan", 3, "Gohan@super.saiya"}
+	b := student{"Gohan", 3, "Gohan@super.saiya"}
 
-	c :=student{name: "videl",email: "videl@daughter.satan"}
+	c := student{name: "videl", email: "videl@daughter.satan"}
 
-	d :=student{age: 20}
+	d := student{age: 20}
 
 	fmt.Println(a)
 	fmt.Println(b)
