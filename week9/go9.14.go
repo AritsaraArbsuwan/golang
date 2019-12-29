dc4da6baf8bf9707b5b864d5a07a9f3913dc95be
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-29 21:20:15 +0700

    ีupdate

--- week9/go9.14.go
+++ week9/go9.14.go
@@ -5,8 +5,18 @@ import "fmt"
 func hello(t interface{}) {
 	fmt.Printf("Hello %T \n", t)
 }
-func hi(t ...interface{})  {
+func hi(t ...interface{}) {
 	fmt.Printf("Hi ")
-	for _, v := rande t {	
+	for _, v := range t {
 		fmt.Println(" %T,", v)
+	}
+	fmt.Println()
+}
+
+func main() {
+	hello(3.14159265359)
+	hello(true)
+	hello("Goku")
+	hi("Gohan", false, 10, 10e15)
+
 }
