012a93dde397c3d99f49e30c013f06642b694e2b
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2019-12-29 19:46:57 +0700

    Update go9.6.go

--- week9/go9.6.go
+++ week9/go9.6.go
@@ -1,7 +1,13 @@
 package main
 
+import "fmt"
+
 type student struct {
 	name  string
 	age   int
 	email string
+}
+
+func (std student) introduce() {
+	fmt.Println("Hello my name is", std.name)
 }
