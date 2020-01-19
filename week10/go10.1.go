7c7a0bb0453e8bfd9c8055ef81baf2386f19cfde
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2020-01-19 19:47:48 +0700

    Update go10.1.go

--- week10/go10.1.go
+++ week10/go10.1.go
@@ -1,9 +1,1 @@
-package myMath
-
-func Average(numbers ...float64) float64{
-	var total float64
-	for_, v := range numbars{
-		total = total + v
-	}
-	return total / float64(len(numbers))
-}
+package main
