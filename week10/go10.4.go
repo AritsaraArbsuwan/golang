32ac2c0d81fe754b15c79c46630ae22975e1b453
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2020-02-15 22:45:20 +0700

    uีpdate

--- week10/go10.4.go
+++ week10/go10.4.go
@@ -3,7 +3,7 @@ package main
 //Finds the average  of a series of numbers
 func Average(numbers ...float64) float64 {
 	 var total float64
-	 for_, v:= range numbers{
+	 for_, v:= range numbers {
 		 total = totel + v
 	 }
 	 return totel / float64(len(numbers))
