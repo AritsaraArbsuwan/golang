b37a26c918a5313e88bc4a40cd0c70a3ef0e1216
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2020-02-16 17:14:07 +0700

    Update go11.6.go

--- week11/go11.6.go
+++ week11/go11.6.go
@@ -12,7 +12,9 @@ func increment(data *int, mutex, wg *sync.WaitGroup) {
 	defer mutex.Unlock()
 	mutex.Lock()
 	*data++
-	fmt.Println(time.Since(start), "Increment to", *data){
+	fmt.Println(time.Since(start), "Increment to", *data)
+}
+func read(data *int,mutex *sync.Mutex, wg *sync.WaitGroup) {
 		start := time.Now()
 		defer wg.Done()
 		defer mutex.Unlock()
@@ -31,5 +33,6 @@ func main(){
 	for i := 0; i < 5; i++ {
 		go read(&data, &mutex, &wg)
 	}
+	wg.Wait()
 }
-}
+
