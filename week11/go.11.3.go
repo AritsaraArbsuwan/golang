a3051ed153ab3590e956c51b88ca16c000c8ba85
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2020-02-16 15:26:41 +0700

    Update go.11.3.go

--- week11/go.11.3.go
+++ week11/go.11.3.go
@@ -7,10 +7,10 @@ import (
 
 func say(txt string) {
 	for i := 0; i < 3; i++ {
-		fmt.Println(time.Now(), ":", i, ":", txt)
+		fmt.Println(time.Now(), ":", i, ":", txt
+		time.Sleep(time.Millisecond)
 	}
 }
-
 func main() {
 	go say("Hello")
 	go say("Hi")
