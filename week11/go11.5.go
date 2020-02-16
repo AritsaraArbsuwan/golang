60c53ee479da41cab18ccaa2b1ee6b638991415f
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2020-02-16 16:16:34 +0700

    Update go11.5.go

--- week11/go11.5.go
+++ week11/go11.5.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"sync"
 	"time"
 )
 
 func say(txt string, sleep time.Duration, wg *sync.WaitGroup) {
 	defer wg.Done()
+	fmt.Println(txt)
+	time.Sleep(time.Millisecond * sleep)
 }
