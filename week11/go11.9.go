7bad77d98cc5da57bb5e2dbf6ddfd7eb515f43e1
AritsaraArbsuwan <57857515+AritsaraArbsuwan@users.noreply.github.com>
2020-02-16 18:01:37 +0700

    Update go11.9.go

--- week11/go11.9.go
+++ week11/go11.9.go
@@ -13,8 +13,9 @@ func printerO(tick, boom <-chan.time.Time) {
 		case <-boom:
 			fmt.Println("BOOM!")
 			return
-        
-
-	
+		default:
+			fmt.Println("   .")
+			time.Sleep(50 * time.Millisecond)
 		}
+	}
 }
